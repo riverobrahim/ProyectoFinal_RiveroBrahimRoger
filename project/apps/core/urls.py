eb3c2a0b802252df884086f0da76a20278e7db64
Brahim Roger Rivero <riverobrahim@gmail.com>
2024-02-28 19:12:19 -0300

    Add Logout

--- project/apps/core/urls.py
+++ project/apps/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.contrib.auth.views import LogoutView
 from .views import *
 
 app_name = "core"
@@ -8,4 +8,6 @@ urlpatterns = [
     path("", index, name="index"),
     path("about/", about, name="about"),
     path("login/", CustomLoginView.as_view(),name="login"),
+    path("logout/", LogoutView.as_view(template_name="core/logout.html"),name="logout"),
+    
 ]
--- project/apps/core/views.py
+++ project/apps/core/views.py
@@ -9,7 +9,7 @@ def index(request):
     return render(request, "core/index.html")
 
 def about(request: HttpRequest) -> HttpResponse:
-    return render(request, "core/abaut.html") 
+    return render(request, "core/about.html") 
 
 
 class CustomLoginView(LoginView):
